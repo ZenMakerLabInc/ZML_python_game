512b37fac4126d6b674e7b77c9a15f282f9db87f
zacharyZML <zachary.anderson@zenmakerlab.com>
2020-05-13 15:48:08 -0700

    updated the setup.py file to include images folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+build_exe_options = {"packages": ["os"], "include_files": ["images\\"]}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -10,9 +10,9 @@ base = None
 #if sys.platform == "win32":
 #    base = "Win32GUI"
 
-setup(  name = "helloworld",
+setup(  name = "mygame",
         version = "0.1",
         description = "sfjkasdf",
         options = {"build_exe": build_exe_options},
-        executables = [Executable("helloworld.py", base=base, icon="pika.ico")])
+        executables = [Executable("mygame.py", base=base, icon="pika.ico")])
         
